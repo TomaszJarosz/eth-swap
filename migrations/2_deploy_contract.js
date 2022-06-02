903d7ac4c88aaf525180856d8c0fdb53750be57c
Tomasz Jarosz <tomaszjarosz1994@gmail.com>
2022-06-02 19:19:33 +0200

    create first contract and first erc20 token

---
+++ migrations/2_deploy_contract.js
@@ -0,0 +1,14 @@
+const Token = artifacts.require("Token");
+const EthSwap = artifacts.require("EthSwap");
+
+module.exports = async function (deployer) {
+    deployer.deploy(Token);
+    const token = await Token.deployed()
+    deployer.deploy(EthSwap);
+    const ethSwap = await EthSwap.deployed()
+
+
+    //Transfer all tokens to EthSwap (1 million)
+    await token.transfer(ethSwap.address, '1000000000000000000000000')
+
+};
