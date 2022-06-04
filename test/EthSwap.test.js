82c32e14abd0a5b7735bfceefb42574edebf297e
Tomasz Jarosz <tomaszjarosz1994@domain.com>
2022-06-04 14:11:49 +0200

    add .prettierignore .solhint.json, format EthSwap.test.js

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -1,42 +1,39 @@
-const Token = artifacts.require('Token')
-const EthSwap = artifacts.require('EthSwap')
+const Token = artifacts.require("Token");
+const EthSwap = artifacts.require("EthSwap");
 
-require('chai')
-    .use(require('chai-as-promised'))
-    .should()
+require("chai")
+    .use(require("chai-as-promised"))
+    .should();
 
 function tokens(n) {
-    return web3.utils.toWei(n, 'ether')
+    return web3.utils.toWei(n, "ether");
 }
 
-contract('EthSwap', (accounts) => {
+contract("EthSwap", (accounts) => {
+    let token, ethSwap;
 
-    let token, ethSwap
+    before(async () => {
+        token = await Token.new();
+        ethSwap = await EthSwap.new();
+        //Transfer all tokens to EthSwap(1 million)
+        await token.transfer(ethSwap.address, tokens("1000000"));
+    });
+    describe("Token deployment", async () => {
+        it("contract has a name", async () => {
+            const name = await token.name();
+            assert.equal(name, "DApp Token");
+        });
+    });
 
-    before(async() => {
-        token = await Token.new()
-        ethSwap = await EthSwap.new()
-                 //Transfer all tokens to EthSwap(1 million)
-        await token.transfer(ethSwap.address, tokens('1000000'))
-    })
-    describe('Token deployment', async() => {
-        it('contract has a name', async() => {
-            const name = await token.name()
-            assert.equal(name, 'DApp Token')
-        })
-    })
+    describe("EthSwap deployment", async () => {
+        it("contract has a name", async () => {
+            const name = await ethSwap.name();
+            assert.equal(name, "EthSwap Instant Exchange");
+        });
 
-    describe('EthSwap deployment', async() => {
-        it('contract has a name', async() => {
-            const name = await ethSwap.name()
-            assert.equal(name, 'EthSwap Instant Exchange')
-        })
-
-        it('contract has tokens', async() => {
-
-            let balance = await token.balanceOf(ethSwap.address)
-            assert.equal(balance.toString(), tokens('1000000'))
-
-        })
-    })
-})
+        it("contract has tokens", async () => {
+            let balance = await token.balanceOf(ethSwap.address);
+            assert.equal(balance.toString(), tokens("1000000"));
+        });
+    });
+});
