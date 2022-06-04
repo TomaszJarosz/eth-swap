4e70587b5d0e5d143930ae52512d9145634c1f5b
Tomasz Jarosz <tomaszjarosz1994@domain.com>
2022-06-04 16:52:34 +0200

    add buyToken() contract with test

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -5,9 +5,25 @@ import "./Token.sol";
 contract EthSwap {
     string public name = "EthSwap Instant Exchange";
     Token public token;
+    uint8 public rate = 100;
+
+    event TokenPurchased(
+        address account,
+        address token,
+        uint256 amount,
+        uint256 rate
+        );
 
     constructor(Token _token) public {
         token = _token;
     }
     
+    function buyTokens() public payable {
+        //Calculate the number of tokens to buy
+        uint256 tokenAmount = msg.value * rate;
+        token.transfer(msg.sender, tokenAmount);
+
+        //Emit an event
+        emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
+    }
 }
